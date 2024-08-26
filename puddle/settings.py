f696f9507f5532ee8c6a8ea69d0ee7d1d225c7d4
Rubi Yehezkel <rubke08@gmail.com>
2024-08-26 17:03:09 +0300

    try

--- puddle/settings.py
+++ puddle/settings.py
@@ -27,8 +27,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
